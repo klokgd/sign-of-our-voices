aa85518fab14e4d34ba1c845a26f0f0250c526f3
IlyuhaCODER1999 <jokzik@gmail.com>
2022-05-11 16:59:16 +0300

    complete task

--- routes/upload.js
+++ routes/upload.js
@@ -2,12 +2,33 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var collectionName = "v-for-vendetta";
 
-router.post("/fileupload",(req, res, next) => {
-    var base64 = req.body.image;
-    var data = base64.replace(/^data:image\/png;base64/, "");
-    var pathForDonwload = path.join(__dirname, '..', 'download', 'newImage.jpg');
-    fs.writeFileSync( pathForDonwload, data, {encoding: 'base64'});
-});
 
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
+    const db = client.db("sign-of-our-voices");
+    userCollection = db.collection(collectionName);
+    router.post("/fileupload", (req, res, next) => {
+        let base64 = req.body.image;
+        let data = base64.replace(/^data:image\/png;base64/, "");
+        let pictureId;
+        let objectToInsert =
+            {
+            "path": "123",
+            };
+        userCollection.insertOne(objectToInsert)
+            .then(result => {
+                //pictureId = "444";
+               console.log(result.insertedId.toString());
+            }).catch(error => console.error(error));
+        pictureId = objectToInsert._id.toString() + ".jpg";
+        let pathForDownload = path.join(__dirname, '..', 'download', pictureId);
+        fs.writeFileSync(pathForDownload, data, {encoding: 'base64'});
+        userCollection.updateOne({_id: objectToInsert._id}, {$set: {path:pathForDownload}})
+            .catch(error => console.error(error));
+    });
+
+
+})
 module.exports = router;
