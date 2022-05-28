cc4276ba5b5e40eafb0ad6da546b0f65cb276b08
IlyuhaCODER1999 <jokzik@gmail.com>
2022-05-28 21:50:26 +0300

    Fixed view

--- routes/collection.js
+++ routes/collection.js
@@ -3,8 +3,29 @@ var path = require('path');
 var router = express.Router();
 const Assemblage = require('../models/assemblage');
 const Pictures = require('../models/picture');
+const multer = require('multer');
+const fileFilter = (req, file, cb) => {
 
-/* GET home page. */
+    if(file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg"||
+        file.mimetype === "image/jpeg"){
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+}
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "public/download/collection_images");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+    }
+});
+
+const upload = multer({storage: storageConfig,  fileFilter: fileFilter});
+
 router.get('/', async function (req, res, next) {
     const collections = await Assemblage.find({})
     res.render('index', {title: 'Hui', collections});
@@ -25,16 +46,27 @@ router.get('/new', function (req, res, next) {
     res.render('new-collection');
 })
 
-router.post('/new', function (req,res,next){
+router.get('/successfully', function (req, res, next) {
+    res.render('successfullyAddingAssemblage');
+})
+
+router.post('/new', upload.single("cover"), async function (req,res,next){
     let assemblageName = req.body.name;
     let assemblageDescription = req.body.description;
-
-    let newAssemblage = new Assemblage({name:assemblageName, description: assemblageDescription});
+    let cover = req.file;
+    if (!cover){
+        res.send("Ошибка при загрузке файла");
+    }
+    let pathForDb = path.join('download/collection_images/', cover.filename);
+    let newAssemblage = new Assemblage({name:assemblageName, description: assemblageDescription, image_path: pathForDb});
 
     newAssemblage.save(function (err){
         if(err) return console.log(err);
         console.log("Ассамбляж добавлен", newAssemblage);
     });
-})
+    res.redirect('successfully');
+});
+
+
 
 module.exports = router;
